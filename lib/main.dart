136c69338d61754adbd009a22a24f51796d6fd0d
QuanT <88959861+Quan987@users.noreply.github.com>
2024-03-01 02:04:13 -0500

    Rename ChangeNotifierProvider parameter to QuizDataList

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:quiz_app/models/quiz_data.dart';
+import 'package:quiz_app/models/quiz_flashcard_list.dart';
 import 'package:quiz_app/main_route.dart';
 
 void main() {
   runApp(
-    ChangeNotifierProvider<QuizData>(
-      create: (_) => QuizData(),
+    ChangeNotifierProvider<QuizDataList>(
+      create: (_) => QuizDataList(),
       child: const MainRoute(),
     ),
   );
